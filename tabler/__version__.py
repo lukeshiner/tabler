1ef7b0e9ef5c319c3d2ade17c7fd5e7f24b5cbc1
Luke Shiner <luke@lukeshiner.com>
2018-06-28 15:10:29 +0100

    Set version to 2.2.2.

--- tabler/__version__.py
+++ tabler/__version__.py
@@ -3,7 +3,7 @@
 __title__ = 'tabler'
 __description__ = 'Simple interface for tabulated data and .csv files'
 __url__ = 'http://github.com/axevalley/tabler.git'
-__version__ = '2.2.1'
+__version__ = '2.2.2'
 __author__ = 'Luke Shiner'
 __author_email__ = 'luke@lukeshiner.com'
 __license__ = 'MIT'
